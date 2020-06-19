d841ef564ad350026fc4b5c732f3febafe5b66b8
yusais_life@qq.com <39078847+helloyuzz@users.noreply.github.com>
2020-06-19 09:11:17 +0800

    Create PKIsNull.cs

---
+++ Fisher.Core/Variable/PKIsNull.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fisherman.Core {
+    public class PKIsNull:Exception {
+        public PKIsNull(string message) : base(message) {
+        }
+    }
+}
